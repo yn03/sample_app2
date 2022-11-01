c6d69d01380963df9a5513c9010edbb5d6e14c90
yn03 <mlkn862n@gmail.com>
2022-11-01 15:28:26 +0000

    7章　削除機能まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
